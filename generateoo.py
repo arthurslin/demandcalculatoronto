69ba5408cd4f078dcfe0ecc6ce8dafb19474f4d4
Arthur Lin <arthurslin.work@gmail.com>
2024-07-22 12:17:21 -0700

    defaultdict implementation

--- generateoo.py
+++ generateoo.py
@@ -2,6 +2,7 @@ import pandas as pd
 import math
 import glob
 import os
+from collections import defaultdict
 from datetime import date
 
 demanddir = 'demandchart'
@@ -36,43 +37,54 @@ def create_report(data):
         "Item Qty"].sum(numeric_only=False).to_dict()
 
     # q_dict = inventory.set_index("Item Number")["Item Qty"].to_dict()
+    demand["Name"] = demand["Name"].astype(str)
     demand_dict = demand.set_index("Name")["Median Demand"].to_dict()
     vel["PART_NUMBER"] = vel["PART_NUMBER"].astype(str)
     vel_dict = vel.set_index("PART_NUMBER")["Event Class"].to_dict()
-    print(vel_dict)
 
     df2 = oor.groupby("Item Code", sort=False)[
         'Quantity Due'].sum(numeric_only=False)
 
     df2d = df2.to_dict()
     oor["Quantity Due"] = oor["Item Code"].apply(lambda x: df2d.get(x))
+    oor["Item Code"] = oor["Item Code"].astype(str)
 
+    demand_dict = defaultdict(int, demand_dict)
+    q_dict = defaultdict(int, q_dict)
+    vel_dict = defaultdict(int, vel)
     for i in range(len(oor)):
-        pn, quantity, price = str(oor.at[i, "Item Code"]), oor.at[i, "Quantity Due"], oor.at[i, "PO Price"]
-        if pd.isna(pn) or pd.isna(quantity):
+        pn, quantity, price = oor.at[i, "Item Code"], oor.at[i,
+                                                             "Quantity Due"], oor.at[i, "PO Price"]
+
+        if pd.isna(pn):
             continue
-        if pn in q_dict and pn in demand_dict:
-            # Subinventory quantity / Monthy Demand mean
-            expected_mos = q_dict[pn]/demand_dict[pn]
-            after_push = q_dict[pn] - quantity
-            oor.at[i, 'Expected MOS'] = round(expected_mos, 2)
-            oor.at[i, 'Inventory Qty - Qty Due'] = after_push
-            oor.at[i, "Median Demand"] = demand_dict[pn]
-            oor.at[i, "Valid Inventory"] = q_dict[pn]
-            thisdate = date.today()
-            new_date = (pd.to_datetime(thisdate) +
-                        pd.DateOffset(days=math.floor(expected_mos*30))).date()
-            oor.at[i, "Month to Zero Inventory"] = new_date
-            action = "Cancel" if expected_mos >= 6 else "Push Out" if 3 <= expected_mos < 6 else "No Action"
-            oor.at[i, "MOS Action"] = action
+        
         oor.at[i, "Part Cost"] = "High $" if price > 5000 else (
             "Low $" if price < 1000 else "Mid $")
+        # if pn in q_dict and pn in demand_dict:
+        # Subinventory quantity / Monthy Demand mean
+        if demand_dict[pn] == 0:
+            expected_mos = 0
+        else:
+            expected_mos = q_dict[pn]/demand_dict[pn]
+
+        if not pd.isna(quantity):
+            after_push = q_dict[pn] - quantity
+            oor.at[i, 'Inventory Qty - Qty Due'] = after_push
+        oor.at[i, 'Expected MOS'] = round(expected_mos, 2)
+        oor.at[i, "Median Demand"] = demand_dict[pn]
+        oor.at[i, "Valid Inventory"] = q_dict[pn]
+        thisdate = date.today()
+        new_date = (pd.to_datetime(thisdate) +
+                    pd.DateOffset(days=math.floor(expected_mos*30))).date()
+        oor.at[i, "Month to Zero Inventory"] = new_date
+
         if pn in vel_dict:
             oor.at[i, "Part Velocity"] = vel_dict[pn]
             oor.at[i, "Part Class"] = (oor.at[i, "Part Cost"]).replace(
                 " Part", "") + " " + oor.at[i, "Part Velocity"]
         else:
-            oor.at[i,"Part Velocity"] = "Zero Demand"
+            oor.at[i, "Part Velocity"] = "Zero Demand"
             oor.at[i, "Part Class"] = (oor.at[i, "Part Cost"]).replace(
                 " Part", "") + " " + oor.at[i, "Part Velocity"]
 
